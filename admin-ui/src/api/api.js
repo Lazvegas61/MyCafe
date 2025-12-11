4b864fbda5b3a0231dd16b6e28099eaa6b1de3ab
Lazvegas61 <volkann.gunaydin@gmail.com>
2025-12-12 02:53:31 +0300

    Delete admin-ui directory

--- admin-ui/src/api/api.js
+++
@@ -1,9 +0,0 @@
-// /home/qw/Desktop/MyCafe/admin-ui/src/api/api.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://127.0.0.1:3000", // NestJS backend adresi
-});
-
-export default api;
-
